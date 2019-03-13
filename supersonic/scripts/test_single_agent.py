0c153e382269c6efa2edfa669c41a3b1f99b47e6
Jake Grigsby <jcg6dn@virginia.edu>
2019-03-13 16:13:17 -0400

    add camera to test_single_agent

--- supersonic/scripts/test_single_agent.py
+++ supersonic/scripts/test_single_agent.py
@@ -2,7 +2,7 @@ import os
 os.chdir('..')
 import argparse
 
-from supersonic import agent
+from supersonic import agent, camera
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,4 +13,10 @@ if __name__ == "__main__":
     sonic = agent.BaseAgent(args.lvl) 
     if args.weights:
         sonic.load_weights(args.weights)
-    sonic.test(1, render=True)
+    cam = camera.Camera(sonic)
+    record_path = 'logs/prototype_agent_00/gameplay/'
+    if not os.path.exists(record_path):
+        os.makedirs(record_path)
+    cam.start_recording(os.path.join(record_path, 'testvid'))
+    sonic.test(1, render=True, max_ep_steps=100)
+    cam.stop_recording()
