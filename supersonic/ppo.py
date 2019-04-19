ea39d76bae7edb18eedaa0714aaffb113e5421c1
Jake Grigsby <jcg6dn@virginia.edu>
2019-04-19 18:06:33 -0400

    updated params

--- supersonic/ppo.py
+++ supersonic/ppo.py
@@ -19,7 +19,7 @@ class PPOAgent:
     """
     Basic version of Proximal Policy Optimization (Clip) with exploration by Random Network Distillation.
     """
-    def __init__(self, env_id, exp_lr=.001, ppo_lr=.00003, vis_model='NatureVision', policy_model='NaturePolicy', val_model='VanillaValue',
+    def __init__(self, env_id, exp_lr=.001, ppo_lr=.00001, vis_model='NatureVision', policy_model='NaturePolicy', val_model='VanillaValue',
                     exp_target_model='NatureVision', exp_train_model='NatureVision', exp_net_opt_steps=8, gamma_i=.99, gamma_e=.999, log_dir=None,
                     rollout_length=128, ppo_net_opt_steps=12, e_rew_coeff=2., i_rew_coeff=1., vf_coeff=.4, exp_train_prop=.25, lam=.95, exp_batch_size=32,
                     ppo_batch_size=32, ppo_clip_value=0.1, update_mean_gae_until=10000, checkpoint_interval=1000, minkl=None, entropy_coeff=.001, random_actions=0):
