fix i_rew normalization

supersonic/ppo.py
@@ -12,7 +12,7 @@ from supersonic import environment, utils, models, logger
 
 class PPOAgent:
     """
-    Basic version of Proximal Policy Optimization (Clip) with exploration by Random Network Distillation.
+    Proximal Policy Optimization (Clip) with exploration by Random Network Distillation.
     """
     def __init__(
         self,
@@ -87,24 +87,10 @@ class PPOAgent:
         self.rollout_length = rollout_length
 
         self.i_rew_running_stats = utils.RunningStats()
+        self.obs_running_stats = utils.RunningMeanStd(shape=self.env.observation_space.shape)
 
         self.log_dir = log_dir
-   
-    def _update_exp_model(self, grads):
-        variables = self.exp_train_model.variables
-        self.exp_optimizer.apply_gradients(
-            zip(grads, variables), global_step=self.global_step,
-        )
-    
-    def _update_ppo_models(self, grads):
-        variables = (
-                self.vis_model.variables
-                + self.policy_model.variables
-                + self.val_model_e.variables
-                + self.val_model_i.variables
-        )
-        self.ppo_optimizer.apply_gradients(zip(grads, variables), global_step=self.global_step)
-
+ 
     def train(self, rollouts, render=0):
         # set up logging
         if self.rank == 0:
@@ -150,13 +136,13 @@ class PPOAgent:
                 self.comm.barrier()
                 #sync weights
                 self.weights = self.comm.bcast(self.weights, root=0)
-            # broadcast new weights to workers
-            if self.stop:
-                break # if early stopping activated
             if self.rank == 0:
                 progbar.update(rollout + 1)
                 if rollout % self.checkpoint_interval == 0:
+                    #save weights
                     self._checkpoint(rollout)
+            if self.stop:
+                break # if early stopping activated
             self.comm.barrier()
         self._checkpoint("final")
 
@@ -178,6 +164,21 @@ class PPOAgent:
                 cum_rew += rew
                 step += 1
         return cum_rew
+
+    def _update_exp_model(self, grads):
+        variables = self.exp_train_model.variables
+        self.exp_optimizer.apply_gradients(
+            zip(grads, variables), global_step=self.global_step,
+        )
+    
+    def _update_ppo_models(self, grads):
+        variables = (
+                self.vis_model.variables
+                + self.policy_model.variables
+                + self.val_model_e.variables
+                + self.val_model_i.variables
+        )
+        self.ppo_optimizer.apply_gradients(zip(grads, variables), global_step=self.global_step)
 
     def _rollout(self, steps, render=False):
         """
@@ -223,68 +224,6 @@ class PPOAgent:
         )
         return trajectory
 
-    def _reset_stats(self):
-        self._log_action_count = [0 for i in range(self.nb_actions)]
-        self._log_cum_rew_e, self._log_cum_rew_i = 0, 0
-        self._log_furthest_point = (0, 0)
-        self._log_death_coords = []
-        self._log_current_lives = 3
-        self._log_episode_num += 1
-        self._log_training_steps = 0
-
-    def _update_ep_stats(self, action, e_rew, i_rew, done, info):
-        self._log_action_count[action] += 1
-        self._log_cum_rew_e += e_rew
-        self._log_cum_rew_i += i_rew
-        self._log_training_steps += 1
-        if self.env_recognized == 'Sonic':
-            if info["lives"] < self._log_current_lives:
-                self._log_current_lives -= 1
-                current_pos = (info["screen_x"], info["screen_y"])
-                self._log_death_coords.append(current_pos)
-                self._log_furthest_point = max(self._log_furthest_point, current_pos)
-            if done:
-                if "FORCED EXIT" in info:
-                    self._log_death_coords = (info["x"], info["y"])
-                    self._log_furthest_point = self._log_death_coords
-                episode_dict = {
-                    "episode_num": self._log_episode_num,
-                    "death_coords": self._log_death_coords,
-                    "training_steps": self._log_training_steps,
-                    "max_x": self._log_furthest_point[0],
-                    "score": info["score"],
-                    "external_reward": self._log_cum_rew_e,
-                    "internal_reward": self._log_cum_rew_i,
-                    "action_count": self._log_action_count,
-                    "max_stage": info["stage"],
-                    "max_act": info["zone"],
-                }
-        
-        elif self.env_recognized == 'Mario':
-            if info["life"] < self._log_current_lives:
-                    self._log_current_lives -= 1
-                    current_pos = (float(info["x_pos"]), float(info["y_pos"]))
-                    self._log_death_coords.append(current_pos)
-                    self._log_furthest_point = max(self._log_furthest_point, current_pos)
-            if done:
-                episode_dict = {
-                    "episode_num": self._log_episode_num,
-                    "death_coords": self._log_death_coords,
-                    "training_steps": self._log_training_steps,
-                    "max_stage": int(info["world"]),
-                    "max_act": int(info["stage"]),
-                    "external_reward": self._log_cum_rew_e,
-                    "internal_reward": self._log_cum_rew_i,
-                    "action_count": self._log_action_count,
-                    "max_x": self._log_furthest_point[0],
-                    "score": int(info["score"]),
-                }
-            
-        if done:
-            episode_log = logger.EpisodeLog(**episode_dict)
-            self.logger.log_episode(episode_log)
-            self._reset_stats()
-
     def _choose_action(self, obs, stochastic=True):
         """
         Choose an action based on the current observation. Saves computation by not running the value net,
@@ -312,23 +251,28 @@ class PPOAgent:
         val_e = tf.squeeze(self.val_model_e(features))
         val_i = tf.squeeze(self.val_model_i(features))
         return tf.log(action_prob), action_idx, val_e, val_i
+    
+    def _normalize_obs(self, obs):
+        """
+        Normalize a *tensor*. Done before RND predictions
+        """
+        obs -= self.obs_running_stats.mean
+        obs /= tf.sqrt(self.obs_running_stats.var)
+        obs = tf.clip_by_value(obs, -5., 5.)
+        return obs
 
     def _calc_intrinsic_reward(self, state):
         """
         reward as described in Random Network Distillation
         """
+        self.obs_running_stats.update(state)
         state = tf.convert_to_tensor(state, dtype=tf.float32)
+        state = self._normalize_obs(state)
         target = self.exp_target_model(state)
         pred = self.exp_train_model(state)
         rew = np.mean(np.square(target - pred))
         return rew, target
-
-    def _checkpoint(self, rollout_num):
-        save_path = f"weights/{os.path.basename(self.log_dir)}/checkpoint_{rollout_num}"
-        if not os.path.exists(save_path):
-            os.makedirs(save_path)
-        self.save_weights(save_path)
-    
+   
     def _make_datasets(self, trajectory, exp_only=False):
         """
         Save time by making the tf datasets once.
@@ -345,10 +289,8 @@ class PPOAgent:
             exp_dataset.shuffle(exp_train_samples + 1)
             .batch(exp_train_samples)
         )
-
         if exp_only:
             return exp_dataset, None
-
         # create new training set
         ppo_dataset = tf.data.Dataset.from_tensor_slices(
             (
@@ -364,7 +306,6 @@ class PPOAgent:
             ppo_dataset.shuffle(trajectory.states.shape[0] + 1)
             .batch(self.rollout_length)
         )
-
         return exp_dataset, ppo_dataset
 
     def _get_grads(self, exp_dataset, ppo_dataset=None):
@@ -374,6 +315,7 @@ class PPOAgent:
         # get exploration network grads
         for state, target in exp_dataset:
             with tf.GradientTape() as tape:
+                state = self._normalize_obs(state)
                 loss = tf.reduce_mean(tf.square(target - self.exp_train_model(state)))
             exp_grads = tape.gradient(loss, self.exp_train_model.variables)
             if self.max_grad_norm:
@@ -442,6 +384,12 @@ class PPOAgent:
         self.val_model_i.load_weights(os.path.join(path, "val_model_i"))
         self.exp_train_model.load_weights(os.path.join(path, "exp_train_model"))
         self.exp_target_model.load_weights(os.path.join(path, "exp_target_model"))
+    
+    def _checkpoint(self, rollout_num):
+        save_path = f"weights/{os.path.basename(self.log_dir)}/checkpoint_{rollout_num}"
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        self.save_weights(save_path)
 
     @property
     def weights(self):
@@ -462,3 +410,65 @@ class PPOAgent:
         self.val_model_i.set_weights(new_weights[3])
         self.exp_target_model.set_weights(new_weights[4])
         self.exp_train_model.set_weights(new_weights[5])
+
+    def _reset_stats(self):
+        self._log_action_count = [0 for i in range(self.nb_actions)]
+        self._log_cum_rew_e, self._log_cum_rew_i = 0, 0
+        self._log_furthest_point = (0, 0)
+        self._log_death_coords = []
+        self._log_current_lives = 3
+        self._log_episode_num += 1
+        self._log_training_steps = 0
+
+    def _update_ep_stats(self, action, e_rew, i_rew, done, info):
+        self._log_action_count[action] += 1
+        self._log_cum_rew_e += e_rew
+        self._log_cum_rew_i += i_rew
+        self._log_training_steps += 1
+        if self.env_recognized == 'Sonic':
+            if info["lives"] < self._log_current_lives:
+                self._log_current_lives -= 1
+                current_pos = (info["screen_x"], info["screen_y"])
+                self._log_death_coords.append(current_pos)
+                self._log_furthest_point = max(self._log_furthest_point, current_pos)
+            if done:
+                if "FORCED EXIT" in info:
+                    self._log_death_coords = (info["x"], info["y"])
+                    self._log_furthest_point = self._log_death_coords
+                episode_dict = {
+                    "episode_num": self._log_episode_num,
+                    "death_coords": self._log_death_coords,
+                    "training_steps": self._log_training_steps,
+                    "max_x": self._log_furthest_point[0],
+                    "score": info["score"],
+                    "external_reward": self._log_cum_rew_e,
+                    "internal_reward": self._log_cum_rew_i,
+                    "action_count": self._log_action_count,
+                    "max_stage": info["stage"],
+                    "max_act": info["zone"],
+                }
+        
+        elif self.env_recognized == 'Mario':
+            if info["life"] < self._log_current_lives:
+                    self._log_current_lives -= 1
+                    current_pos = (float(info["x_pos"]), float(info["y_pos"]))
+                    self._log_death_coords.append(current_pos)
+                    self._log_furthest_point = max(self._log_furthest_point, current_pos)
+            if done:
+                episode_dict = {
+                    "episode_num": self._log_episode_num,
+                    "death_coords": self._log_death_coords,
+                    "training_steps": self._log_training_steps,
+                    "max_stage": int(info["world"]),
+                    "max_act": int(info["stage"]),
+                    "external_reward": self._log_cum_rew_e,
+                    "internal_reward": self._log_cum_rew_i,
+                    "action_count": self._log_action_count,
+                    "max_x": self._log_furthest_point[0],
+                    "score": int(info["score"]),
+                }
+            
+        if done:
+            episode_log = logger.EpisodeLog(**episode_dict)
+            self.logger.log_episode(episode_log)
+            self._reset_stats()

supersonic/utils.py
@@ -119,14 +119,12 @@ class Trajectory:
 
         # object used to keep track of running variance. Passed between PPOAgent and Trajectory.
         self.i_rew_running_stats = i_rew_running_stats
-
         self.rollout_length = rollout_length
 
     def add(self, state, rew_e, rew_i, exp_target, act_prob_tuple, val_e, val_i, done):
         self.states.append(np.squeeze(state))
         self.rews_e.append(rew_e)
         self.rews_i.append(rew_i)
-        self.i_rew_running_stats.push(rew_i)  # update running variance statistic
         self.old_act_probs.append(act_prob_tuple[0])
         self.actions.append(act_prob_tuple[1])
         self.vals_e.append(val_e)
@@ -153,8 +151,13 @@ class Trajectory:
         self.vals_e.append(last_val_e)
         self.vals_i.append(last_val_i)
         self._lists_to_ndarrays()
-        # normalize internal rews, but shrink them to same scale as external rewards
-        self.rews_i = .01 * self.normalize_rews_i(self.rews_i)
+
+        # normalize internal rews
+        rff = RewardForwardFilter(gamma_i)
+        rffs = [rff.update(rew) for rew in self.rews_i]
+        for rew in rffs:
+            self.i_rew_running_stats.push(rew)
+        self.rews_i = self.normalize_rews_i(self.rews_i)
 
         #calculate internal returns/advantages
         gae = np.zeros(())
@@ -178,6 +181,18 @@ class Trajectory:
         self.rets_e = discounted_return_e.astype(np.float32)
         self.rets_i = discounted_return_i.astype(np.float32)
         self.gaes = (e_rew_coeff * adv_e + i_rew_coeff * adv_i).astype(np.float32)
+
+class RewardForwardFilter(object):
+    def __init__(self, gamma):
+        self.rewems = None
+        self.gamma = gamma
+
+    def update(self, rews):
+        if self.rewems is None:
+            self.rewems = rews
+        else:
+            self.rewems = self.rewems * self.gamma + rews
+        return self.rewems
 
 #########################################################################################
 
@@ -240,6 +255,34 @@ class RunningStats:
     def standard_deviation(self):
         return math.sqrt(self.variance())
 
+class RunningMeanStd(object):
+    # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+    def __init__(self, epsilon=1e-4, shape=()):
+        self.mean = np.zeros(shape, 'float32')
+        self.var = np.ones(shape, 'float32')
+        self.count = epsilon
+
+    def update(self, x):
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self.update_from_moments(batch_mean, batch_var, batch_count)
+
+    def update_from_moments(self, batch_mean, batch_var, batch_count):
+        delta = batch_mean - self.mean
+        tot_count = self.count + batch_count
+
+        new_mean = self.mean + delta * batch_count / tot_count
+        m_a = self.var * (self.count)
+        m_b = batch_var * (batch_count)
+        M2 = m_a + m_b + np.square(delta) * self.count * batch_count / (self.count + batch_count)
+        new_var = M2 / (self.count + batch_count)
+
+        new_count = batch_count + self.count
+
+        self.mean = new_mean
+        self.var = new_var
+        self.count = new_count
 #######################################################################################
 
 def moving_average(x, w):